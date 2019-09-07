5928e1930711b9ed0c308560068a38ca087d7bfe
unknown <paraxite@naver.com>
2019-09-07 19:49:07 +0900

    use build.rs to generate static index.html string

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,51 +2,17 @@ use std::sync::mpsc::Sender;
 use web_view::*;
 use crate::message::{SoundMessage, VolumeChange};
 
+static HTML: &str = include_str!(concat!(env!("OUT_DIR"), "/index.html"));
+
 pub fn run(
     tx: Sender<SoundMessage>,
     gamelog_path: Option<std::path::PathBuf>,
     soundpack_path: Option<std::path::PathBuf>,
     ignore_path: Option<std::path::PathBuf>,
 ) {
-    let html = format!(
-        r#"
-        <!doctype html>
-        <html>
-            <head>
-                <style type="text/css">{w3}</style>
-                <style type="text/css">{range}</style>
-            </head>
-            <body>
-                <div class="w3-bar w3-border w3-light-grey w3-small">
-                    <button class='w3-bar-item w3-button'
-                        onclick="external.invoke('load_gamelog')">Load gamelog.txt</button>
-                    <button class='w3-bar-item w3-button'
-                        onclick="external.invoke('load_soundpack')">Load soundpack</button>
-                    <button class='w3-bar-item w3-button'
-                        onclick="external.invoke('load_ignore_list')">Load ignore.txt</button>
-                    <div class='w3-dropdown-hover w3-right'>
-                        <a ref ='#' class='w3-button'>Options</a>
-                        <div class='w3-dropdown-content w3-bar-block' style='right:0'>
-                            <button class='w3-bar-item w3-button w3-disabled'><s>Download Original's Soundpack</s></button>
-                            <button class='w3-bar-item w3-button w3-disabled'><s>Set current paths as default</s></button>
-                            <button class='w3-bar-item w3-button w3-disabled'><s>Set current volumes as default</s></button>
-                            <button class="w3-bar-item w3-button"
-                                onclick="external.invoke('show_about')">About</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="w3-container">
-                    <table class="w3-table w3-bordered" id="channels"></table>
-                </div>
-            </body>
-        </html>
-        "#,
-        w3 = include_str!("w3.css"),
-        range = include_str!("range.css"),
-    );
     let webview = builder()
         .title("SoundSense-rs")
-        .content(Content::Html(html))
+        .content(Content::Html(HTML))
         .size(500, 550)
         .resizable(true)
         .debug(true)
@@ -70,12 +36,13 @@ pub fn run(
                 }
                 "show_about" => {
                     webview.dialog()
-                        .info("SoundSense-rs",
+                        .info(
+"SoundSense-rs",
 r"Created by prixt
 The original SoundSense can be found at:
-  http://df.zweistein.cz/soundsense/
+    http://df.zweistein.cz/soundsense/
 Source at:
-  https://github.com/prixt/soundsense-rs"
+    https://github.com/prixt/soundsense-rs",
                         ).unwrap()
                 }
                 other => {
@@ -122,33 +89,33 @@ impl UIHandle {
             self.handle.dispatch(
                 move |webview| {
                     let script = format!(
-                    r#"
-                    let channels = document.getElementById('channels');
-                    channels.insertAdjacentHTML(
-                        'beforeend',
-                        "<tr class='w3-row'> \
-                            <td class='w3-center' style='width:50px'><h4>{channel_name}</h4></td> \
-                            <td class='w3-rest'> \
-                                <input type='range' \
-                                    name='{channel_name}_slider' \
-                                    id='{channel_name}_slider' \
-                                    min='0' \
-                                    max='100' \
-                                    value='100' \
-                                /> \
-                            </td> \
-                        </tr>"
-                    );
+r#"
+let channels = document.getElementById('channels');
+channels.insertAdjacentHTML(
+    'beforeend',
+    "<tr class='w3-row'> \
+        <td class='w3-center' style='width:50px'><h4>{channel_name}</h4></td> \
+        <td class='w3-rest'> \
+            <input type='range' \
+                name='{channel_name}_slider' \
+                id='{channel_name}_slider' \
+                min='0' \
+                max='100' \
+                value='100' \
+            /> \
+        </td> \
+    </tr>"
+);
 
-                    let slider = document.getElementById("{channel_name}_slider");
-                    slider.addEventListener(
-                        /MSIE|Trident|Edge/.test(window.navigator.userAgent) ? 'change' : 'input',
-                        function() {{
-                            external.invoke('{{"channel":"{channel_name}", "volume":'+this.value+'}}');
-                        }},
-                        false
-                    );
-                    "#,
+let slider = document.getElementById("{channel_name}_slider");
+slider.addEventListener(
+    /MSIE|Trident|Edge/.test(window.navigator.userAgent) ? 'change' : 'input',
+    function() {{
+        external.invoke('{{"channel":"{channel_name}", "volume":'+this.value+'}}');
+    }},
+    false
+);
+"#,
                     channel_name=&name);
                     webview.eval(&script)
                 }
@@ -159,12 +126,12 @@ impl UIHandle {
         self.handle.dispatch(
             |webview| {
                 webview.eval(
-                    r#"
-                    let channels = document.getElementById("channels");
-                    while (channels.firstChild) {
-                        channels.removeChild(channels.firstChild);
-                    }
-                    "#
+r#"
+let channels = document.getElementById("channels");
+while (channels.firstChild) {
+    channels.removeChild(channels.firstChild);
+}
+"#
                 )
             }
         ).unwrap();
